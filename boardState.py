ab139c36957c03f94bfc47ddb9171b3a82a7c668
blkkap <dovala69@gmail.com>
2026-01-19 12:45:25 -0800

    STATE/DS

--- boardState.py
+++ boardState.py
@@ -1,5 +1,5 @@
 import chess
-
+import numpy as np
 
 
 class boardState(object):
@@ -19,21 +19,21 @@ class boardState(object):
         bstate =np.zeros(64, np.uint8)
         for i in range(64):
             pp = self.board.piece_at(i)
-            if pp is None:
+            if pp is not None:
                 bstate[i] = {"P": 1, "N": 2, "B": 3, "R": 4, "Q": 5, "K": 6, \
                              "p": 9, "n":10, "b":11, "r":12, "q":13, "k": 14}[pp.symbol()]
 
         if self.board.has_queenside_castling_rights(chess.WHITE):
-            assert bstate[0] = 4
+            assert bstate[0] == 4
             bstate[0] = 7
-        if self.board.has_queenside_castling_rights(chess.WHITE):
-            assert bstate[7] = 4
+        if self.board.has_kingside_castling_rights(chess.WHITE):
+            assert bstate[7] == 4
             bstate[7] = 7
         if self.board.has_queenside_castling_rights(chess.BLACK):
-            assert bstate[56] = 12
+            assert bstate[56] == 12
             bstate[56] = 15
-        if self.board.has_queenside_castling_rights(chess.BLACK):
-            assert bstate[63] = 12
+        if self.board.has_kingside_castling_rights(chess.BLACK):
+            assert bstate[63] == 12
             bstate[63] = 15
 
         if self.board.ep_square is not None:
@@ -42,7 +42,7 @@ class boardState(object):
         bstate = bstate.reshape(8,8)
         
 
-        state = np.zeros((5,8,8), np.unit8)
+        state = np.zeros((5,8,8), np.uint8)
 
         state[0] = (bstate>>3)&1
         state[1] = (bstate>>2)&1
--- data.py
+++ data.py
@@ -1,57 +1,41 @@
 import chess
 import chess.pgn
-from io import StringIO
+from boardState import boardState
+import numpy as np
 
 def generate_train_set(numLines=None):
     values = {'1/2-1/2':0, '0-1':-1, '1-0':1}
-    games = []
+    gn = 0
+    X, y =  [],[]
+
     with open('data/lichess_db_standard_rated_2019-05.pgn') as pgn:
-            for i in range(numLines):
-
-                game = chess.pgn.read_game(pgn)
-                if game is None:
-                    break
-                print(game)
-                res = game.headers['Result']
-                if res not in values:
-                    continue
-                value = values[res]
-                board = game.board()
-            
-                for move in game.mainline_moves():
-                    board.push(move)
-                    if (board.is_game_over()):
-                        print("-----------",res)
-                print(board,"\n")
-                print(board.fen())
-
-
-'''
-def test_data(x=None):
-    with open('data/lichess_db_standard_rated_2019-05.pgn') as pgn:
-        games = []
-
-        for i in range(x):
+        while True:
             game = chess.pgn.read_game(pgn)
             if game is None:
                 break
-            games.append(game)
-            print(games)
-    
-            for s in games:
-                g = chess.pgn.read_game(s)
-                g.headers['Result']
-                board = g.board()
-                for move in g.main_line():
-                    board.push(move)
-
-            print(games)
-            print(board)
-
-'''
-
+           # print(game)
+            res = game.headers['Result']
+            if res not in values:
+                continue
+            value = values[res]
+            board = game.board()
+            
+            for i, move in enumerate(game.mainline_moves()):
+                board.push(move)
+                ser = boardState(board).serialize()
+                X.append(ser)
+                y.append(value)
+            print("parsing game %d, got %d examples" % (gn, len(X)))
+            if numLines is not None and len(X) > numLines:
+                return X,y
+            gn += 1
+        X = np.array(X)
+        y = np.array(y)
+        return X,y
+                
 
 if __name__ == "__main__":
 
   # test_data(2)
-    generate_train_set(3)
+    X,y = generate_train_set(100000)
+    
